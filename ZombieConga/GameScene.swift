d989037959bf625334bc784b80e49e12bcaca682
Frank Thamel <frankthamel@franks-imac.local>
2016-07-31 22:01:13 +0530

    Completed chapter 1

--- ZombieConga/GameScene.swift
+++ ZombieConga/GameScene.swift
@@ -11,6 +11,16 @@ import SpriteKit
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
+        let background = SKSpriteNode(imageNamed: "background1")
+        background.position = CGPoint(x: size.width / 2 , y: size.height / 2)
+        background.zPosition = -1
+        addChild(background)
+        
+        //add the zombie
+        let zombie = SKSpriteNode(imageNamed: "zombie1")
+        zombie.position = CGPoint(x: 400, y: 400)
+        //zombie.setScale(2)
+        addChild(zombie)
     
     }
     
